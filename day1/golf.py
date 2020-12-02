3192323505df0efb5966e9b48fcdc9348a6f8b32
Torkel Velure <torkel93@gmail.com>
2020-12-02 10:15:38 +0100

    py

--- day1/golf.py
+++ day1/golf.py
@@ -1,3 +1,5 @@
+r = "1000\n1000\n20\n1020" #Some input with numbers separated by newlines
+
 from itertools import combinations 
 from math import prod
 s = lambda r,n: prod([x for x in combinations([int(k) for k in r.splitlines()],n) if sum(x)==2020][0])
