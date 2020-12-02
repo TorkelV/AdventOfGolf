ccb4334e40b38318bed2dc801a5b915bb762da7d
Torkel Velure <torkel93@gmail.com>
2020-12-02 10:14:06 +0100

    more solutions

---
+++ day1/golf.py
@@ -0,0 +1,6 @@
+from itertools import combinations 
+from math import prod
+r = "1000\n1000\n20\n1020"
+s = lambda r,n: prod([x for x in combinations([int(k) for k in r.splitlines()],n) if sum(x)==2020][0])
+part1 = s(r,2)
+part2 = s(r,3)
