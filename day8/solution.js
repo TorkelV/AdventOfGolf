3374d191663d32f9a948e21507e54f8869540589
Torkel Velure <torkel93@gmail.com>
2020-12-08 14:38:24 +0100

    oof

---
+++ day8/solution.js
@@ -0,0 +1,19 @@
+var parse = r => r.trim().split("\n").map(e=>e)
+
+var part1=input=>{
+    var r = parse(input)
+
+    return r
+}
+
+
+var exInput = ``
+
+console.log("Example solution:")
+console.log(part1(exInput))
+
+
+var input = ``
+
+console.log("Solution:")
+console.log(part1(input))
