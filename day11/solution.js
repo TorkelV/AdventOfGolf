6086bb995e0340dc49a6e6b3fb3f7ae1f48fcb50
Torkel Velure <torkel93@gmail.com>
2020-12-11 10:58:51 +0100

    trash

---
+++ day11/solution.js
@@ -0,0 +1,22 @@
+var parse = r => {
+    return r.split("\n").map(e=>e)
+}
+
+var part1 = input => {
+    var r = parse(input)
+    
+    return r
+}
+
+
+
+var exInp = ``
+
+console.log("example1:")
+console.log(part1(exInp))
+
+
+var inp = `` 
+
+console.log("solution:")
+console.log(part1(inp))
