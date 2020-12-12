e1de32fb5ada3cc3de16db5fa7dc179cde461233
Torkel Velure <torkel93@gmail.com>
2020-12-12 07:18:40 +0100

    wtf

---
+++ day12/solution.js
@@ -0,0 +1,24 @@
+var parse = r => {
+    return r.split("\n").map(e=>e.split(""))
+}
+
+var part1 = input => {
+    var r = parse(input)
+
+    return r
+}
+
+
+
+
+
+var exInp = ``
+
+console.log("example1:")
+console.log(part1(exInp))
+
+
+var inp = `` 
+
+console.log("solution:")
+console.log(part1(inp))
